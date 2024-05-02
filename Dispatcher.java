2bff2170c0049376d83c3ea5a2c2a4a28d11a4a8
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-05-02 22:33:17 +0400

    Update Dispatcher.java

--- Dispatcher.java
+++ Dispatcher.java
@@ -2,23 +2,23 @@ public class Dispatcher {
 
   public static void main(String[] args) {
     int x = 10;
-    System.out.println(x);
+ // System.out.println(x);
     x = changeAndDisplay(x);
-    System.out.println(x);
+//  System.out.println(x);
 
     Car c = new Car();
     c.price = 4500;
     c.enginePrice = 2500;
     changeObject(c);
-    c.price = changeAndDisplay(c.price);
-    c.enginePrice = changeAndDisplay(c.enginePrice);
+ //   c.price = changeAndDisplay(c.price);
+ //   c.enginePrice = changeAndDisplay(c.enginePrice);
     System.out.println(c.price);
     System.out.println(c.enginePrice);
   }
 
   static int changeAndDisplay(int x) {
     x = x * 2;
-    System.out.println(x);
+//    System.out.println(x);
     return x;
 
   }
